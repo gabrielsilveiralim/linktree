866a1caaf1b4b12c0754b79a96ccb9c2121f69fa
unknown <gabriel14ima@gmail.com>
2026-02-28 12:49:56 -0300

    feat: initial projet

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import About from "../components/about/About";
+import Header from "../components/header/Header";
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <About />
+    </>    
+  );
+}
